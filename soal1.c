670c4c34076cac448d6363855bcf6b478afdaba4
aisyahd01 <aisyahdahayu02@gmail.com>
2026-06-04 08:28:51 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    return 0;
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+Node *head = NULL;
+
+void pushFront(int x) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = x;
+    newNode->next = head;
+    head = newNode;
+}
+
+void pushBack (int x) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data=x;
+    newNode->next= NULL;
+    
+    if (head==NULL){
+        head=newNode;
+        return;
+    }
+    Node *temp= head;
+    while (temp->next!=NULL){
+        temp=temp->next;
+    }
+    
+    
+
+
+
+
 }
