dbe23f0ae2fb42ca7293117a540abe2fb84290cc
aisyahd01 <aisyahdahayu02@gmail.com>
2026-06-04 09:04:39 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -29,9 +29,99 @@ void pushBack (int x) {
         temp=temp->next;
     }
     
+    temp->next=newNode;
+}
+
+
+void Delete (int x){
+    Node *temp = head;
+    Node *prev = NULL;
+
+    while(temp!=NULL && temp->data!=x){
+        prev=temp;
+        temp=temp->next;
+    }
+    
+    if (temp==NULL) {
+        return;
+    }
+    if (prev==NULL){ 
+        head= head->next;
+    }
+    else {
+        prev->next=temp->next;}
+
+    free(temp);
+}
+
+void Find (int x) {
+    Node *temp=head;
+    int posisi=0;
+
+    while (temp!=NULL) {
+        if (temp->data==x) {
+            printf("FOUND %d\n", posisi);
+            return;
+        }
+        temp =temp->next;
+        posisi++;
+    }
+
+    printf("NOT FOUND\n");
+}
+
+void printList (){
+    if (head==NULL){
+        printf("LIST EMPTY\n");
+        return;
+    }
+
+    printf ("LIST");  
+    Node *temp=head;
+    while (temp!=NULL) {
+        printf(" %d", temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+int main (){
+    int Q;
+    scanf ("%d", Q);
+
+    for (int i=0; i<Q; i++){
+        int op, x;
+        scanf ("%d %d", &op, &x);
+
+        if (op==1){
+            pushFront(x);
+        }
+        else if (op==2){
+            pushBack(x);
+        }
+        else if (op==3){
+            Delete(x);
+        }
+        else if (op==4){
+            Find (x);
+        }
+    }
+
+    printList();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
     
 
+    
 
-
-
-}
